6bc0b6667e39ec5c0113032bd044884e73d38afb
Magela_Cornelio <gelacornelio3@gmail.com>
2023-07-19 11:55:09 -0400

    proyecto actualizado

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -3,14 +3,18 @@
 
 public static class Moogle
 {
-    public static/* SearchResult*/ void Query(string query)
+    public static SearchResult Query(string query)
+
     {
+        SearchItem[] items = new SearchItem[] { };
+
         //guardar las direcciones de los documentos. 
         //path.join concatena varios elementos unidos de un (/)
         //directory.getfiles devuelve una cadena con todas las direcciones separados por el .txt.
+
         string Address = Path.Join(Directory.GetParent(Directory.GetCurrentDirectory())?.FullName, "content");
         string[] Documents_Address = Directory.GetFiles(Address, ".txt*");
-
+        string Title = Address.Substring(Address.LastIndexOf(@"\") + 1);
 
         // guardar en un array de string todos los documentos
         string[] Documents = new string[Documents_Address.Length];
@@ -19,32 +23,146 @@ public static class Moogle
             Documents[i] = File.ReadAllText(Documents_Address[i]);
         }
 
-        // crear un jagged donde cada subarray es un documento 
-        string[][] All_Documents = new string[Documents.Length][];
+        // creo una lista de lista donde cada sublista es un documento 
+        List<List<string>> All_Documents = new();
         for (int i = 0; i < Documents.Length; i++)
         {
-            All_Documents[i] = Documents[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            All_Documents[i].Add(Documents[i]);
 
         }
+        //creo un diccionario de los titulos con sus documentos
+        Dictionary<string, string> Title_Of_Each_Document = Extras.Title_Of_Each_Document(Documents_Address);
 
         // creo una lista que guarde todas las palabras de los documentos creando asi el Vocabulario.
-        List<string> all_documents = new();
-        //Metodo que normaliza la lista, quita signos de puntuacion, mayusculas
-        // tildes, dejo solo letras y numeros 
-        Extras.Extras.Normalize_Text(all_documents);
+        List<string> Vocabulary = new();
+        for (int i = 0; i < Documents.Length; i++)
+        {
+            Vocabulary.AddRange(All_Documents[i]);
+        }
+        //Metodo que normaliza la lista, quita signos de puntuacion, mayusculas, tildes, dejo solo letras y numeros 
+        Extras.Normalize_Text(Vocabulary);
         //elimino los elementos repetidos de la lista
-        all_documents.Distinct();
+        Vocabulary.Distinct();
 
 
         //guardamos el query en una lista y lo normalizamos
-        List<string> Query = new();
-        Extras.Extras.Normalize_Text(Query);
+        List<string> Query = query.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+        Extras.Normalize_Text(Query);
+        //creamos un diccionario que seran el idf de todas las palabras del vocabulario
+        var Dictionary_IDF_Words = Extras.Convert_To_Diccionary_IDF(Vocabulary, All_Documents);
+        //guardamos en lista el tf y el idf del query para luego multiplicarlo y crear asi nuestro vector query
+        List<double> TF_Query = Extras.TF_Query(Query, Vocabulary);
+        List<double> IDF_Query = Extras.IDF_Query(Query, Dictionary_IDF_Words);
+        List<double> TF_IDF_Query = new();
+        for (int h = 0; h < TF_Query.Count; h++)
+        {
+            TF_IDF_Query.Add(TF_Query[h] * IDF_Query[h]);
+        }
 
-     
+        //creamos una lista  de lista que sera nuestra matriz con el tf por el idf de cada palabra en cada documento
+        List<List<double>> matrix_TF_IDF = new List<List<double>>();
+        //matrix_TF_IDF.Add(Extras.Extras.Matrix_TF_IDF(All_Documents, Vocabulary));
+
+
+        //calculamos las magnitudes de los vectores(documentos) para empezar a calcular la similitud de coseno
+        double Norm = 0;
+        for (int k = 0; k < matrix_TF_IDF.Count; k++)
+        {
+            Norm = Extras.Vector_Magnitude(matrix_TF_IDF[k]);
+        }
+        //le calculamos la magnitud al query
+        double Norm_Query = Extras.Vector_Magnitude(TF_IDF_Query);
+        //guardamos en un diccionario el resultado de la similitud de coseno con su respectivo documento
+        Dictionary<double, string> Dictionary_PreScore = new Dictionary<double, string>();
+        double Score = 0;
+        List<double> Score_Docs = new();
+        for (int m = 0; m < All_Documents.Count; m++)
+        {
+            for (int j = 0; j < All_Documents.Count; j++)
+            {
+                Score = Extras.Cosine_Similarity(matrix_TF_IDF[j], TF_IDF_Query, Norm, Norm_Query);
+                Score_Docs.Add(Score);
+                Dictionary_PreScore.Add(Score, All_Documents[m][j]);
+            }
+
+
+        }
+        //ordeno la lista  del score de mayor a menor y  guradamos en un diccionario los 
+        //documento con su score de mayor a menor
+        Score_Docs.Sort();
+        Dictionary<string, double> Dictionary_Score = new Dictionary<string, double>();
+        for (int p = 0; p < Score_Docs.Count; p++)
+        {
+            Dictionary_Score.Add(Dictionary_PreScore[Score_Docs[p]], Score_Docs[p]);
+        }
+        //creamos una lista que seran los documentos que devolveremos de mayor a menor
+        List<string> Return_Documents = Dictionary_Score.Keys.Take(10).ToList();
+        //creamos uns lista de lista para el snippet, donde cada sublista sera una oracion del documento
+        //cuales documentos seran la lista creada anteriormente de los documentos devueltos
+        List<List<string>> Snippet = Extras.Matrix_Snippet(Return_Documents);
+        //tenemos ahora la matriz del snippet con el tf y el idf calculado
+        List<List<double>> Snippet_TF_IDF = Extras.Snippet_TF_IDF(Snippet, Vocabulary);
+        //calculamos la magnitud de los vectores(oraciones) del snippet 
+        double Norm_Snippet = 0;
+        for (int i = 0; i < Snippet_TF_IDF.Count; i++)
+        {
+            Norm_Snippet = Extras.Vector_Magnitude(Snippet_TF_IDF[i]);
+        }
+        //calculamos la similutud de coseno entre el snippet, guardando en un diccionario la oracion con su
+        // respectivo score
+        Dictionary<double, string> Pre_Snippet_Score = new Dictionary<double, string>();
+        double Score_Snippet = 0;
+        List<double> Score_Snippet_List = new();
+        for (int i = 0; i < Snippet.Count; i++)
+        {
+            for (int j = 0; j < Snippet.Count; j++)
+            {
+                Score_Snippet = Extras.Cosine_Similarity(Snippet_TF_IDF[j], TF_IDF_Query, Norm_Snippet, Norm_Snippet);
+                Score_Snippet_List.Add(Score_Snippet);
+                Pre_Snippet_Score.Add(Score_Snippet, Snippet[i][j]);
+            }
+        }
+        //ordeno la lista de mayor a menor y guardo en un diccionario las oraciones con su score de mayor a menor
+        Score_Snippet_List.Sort();
+        Dictionary<string, double> Score_Snippet_Oficial = new Dictionary<string, double>();
+        for (int i = 0; i < Score_Snippet_List.Count; i++)
+        {
+            Score_Snippet_Oficial.Add(Pre_Snippet_Score[Score_Snippet_List[i]], Score_Snippet_List[i]);
+
+        }
+        //creamos un string con la oracion que mas score tenga y esa sera la que entreguemos 
+        List<string> Finalist = Score_Snippet_Oficial.Keys.Take(1).ToList();
+        string snippet = Finalist[0];
+        string[] titles = new string[Return_Documents.Count];
+        double[] score = new double[Score_Docs.Count];
+        //diccionario que tiene los titulos de los documentos a entregar
+        Dictionary<string, string> Documents_Title_Deliver = Extras.Documents_with_Title(Return_Documents, Title_Of_Each_Document);
+        for (int i = 0; i < Score_Docs.Count; i++)
+        {
+            titles[i] = Documents_Title_Deliver[Return_Documents[i]];
+            score[i] = Score_Docs[i];
+        }
+        SearchItem[] Almost_Last = Extras.Result(titles, score, snippet, Return_Documents);
 
 
 
 
-        // return new SearchResult(items, query);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return new SearchResult(Almost_Last,);
     }
 }
--- MoogleEngine/extras.cs
+++ MoogleEngine/extras.cs
@@ -1,7 +1,23 @@
 
-namespace Extras;
+namespace MoogleEngine;
 public static class Extras
 {
+
+    //metodo que devuelve un diccionario de cada documento y su titulo
+    public static Dictionary<string, string> Title_Of_Each_Document(string[] documents)
+    {
+        Dictionary<string, string> Title_Of_Each_Document = new Dictionary<string, string>();
+        string title;
+        string document;
+        for (int i = 0; i < documents.Length; i++)
+        {
+            title = documents[i].Substring(documents[i].LastIndexOf(@"\") + 1);
+            document = File.ReadAllText(documents[i]);
+            Title_Of_Each_Document.Add(document, title);
+        }
+        return Title_Of_Each_Document;
+
+    }
     //metodo que normaliza los textos quitando mayusculas(ToLower),
     // signos de puntuacion()
     public static void Normalize_Text(List<string> example)
@@ -17,7 +33,7 @@ public static class Extras
             item.Replace(".", " ");
             item.Replace(";", " ");
             item.Replace(":", " ");
-            //dejo solamente leyras y numeros en la lista
+            //dejo solamente letras y numeros en la lista
             foreach (char index in item)
             {
                 if (!Char.IsLetterOrDigit(index))
@@ -28,47 +44,204 @@ public static class Extras
         }
 
     }
-
-    //metodo que me devuelve la cantidad de dodcumentos
-    public static int Count_Of_Documents(List<List<string>> example)
-    {
-        int count = example.Count;
-        return count;
-    }
-    //metodo booleano que me confirme si la palabra esta en el documento
-    public static void Word_in_the_Document(string example)
-    {
-
-    }
-
     //metodo que analiza la cantidad de veces que se repite
-    // una palabra en un documento(TF)
-    public static void/* double[][]*/ Term_Frequency(string[][] documents, List<string> Vocabulary)
+    // una palabra en un documento(TF) 
+    public static double Term_Frequency(List<List<string>> documents, string example)
     {
-        double[][] Matrix_TF = new double[documents.Length][];
         double count = 0;
-        for (int i = 0; i < documents.Length; i++)
+        for (int i = 0; i < documents.Count; i++)
         {
-            for (int j = 0; j < Vocabulary.Count; j++)
+            for (int j = 0; j < documents[i].Count; j++)
             {
-                if (j == Vocabulary.Count)
+                if (documents[i][j] == example)
                 {
-                    Matrix_TF[i][j] = count;
+                    count += 1;
                 }
-                if (documents[i][j] == Vocabulary[j])
-                {
-                    count++;
-
-                }
-
             }
         }
+        return count;
+    }
+    //metodo que calcula la cantidad de docuemntos en los que aparece una palabra del vocabulario
+    public static double Count_Documents_By_Word(List<List<string>> documents, string example)
+    {
+        double count = 0;
+
+        for (int i = 0; i < documents.Count; i++)
+        {
+            for (int j = 0; j < documents[i].Count; j++)
+            {
+                if (example == documents[i][j])
+                {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+    //metodo para saber la cantidad de documentos
+    public static double Count_Of_Documents(List<List<string>> documents)
+    {
+        double count = documents.Count;
+        return count;
+    }
+    // metodo que calcula el idf de cada palabra en cada documento
+    private static double Inverse_Document_Frencuency(List<List<string>> documents, string example)
+    {
+        double Result = 0;
+        double Cantidad_Documentos = Count_Of_Documents(documents);
+        double Cantidad_Documentos_Por_Palabras = Count_Documents_By_Word(documents, example);
+        Result = Math.Log(Cantidad_Documentos / Cantidad_Documentos_Por_Palabras);
+        return Result;
+    }
+    //metodo que devuelve un diccionario con el idf de todas las palabras.
+    public static Dictionary<string, double> Convert_To_Diccionary_IDF(List<string> vocabulary, List<List<string>> documents)
+    {
+        double IDF = 0;
+        int count = 0;
+        for (int i = 0; i < vocabulary.Count; i++)
+        {
+            IDF = Inverse_Document_Frencuency(documents, vocabulary[i]);
+            count = i;
+        }
+        var Dictionary_IDF = new Dictionary<string, double>();
+        Dictionary_IDF.Add(vocabulary[count], IDF);
+        return Dictionary_IDF;
+
+    }
+    //metodo que devuelve una lista de lista donde cada sublista es un documento y cada elemento
+    //de cada lista es el tf por el idf de las palabras de ese documento
+    public static List<List<double>> Matrix_TF_IDF(List<List<string>> documents, List<string> Vocabulary)
+    {
+        List<List<double>> Matrix_Result = new List<List<double>>();
+        double tf = 0;
+        var idf = Convert_To_Diccionary_IDF(Vocabulary, documents);
+        for (int i = 0; i < Vocabulary.Count; i++)
+        {
+            tf = Term_Frequency(documents, Vocabulary[i]);
+            Matrix_Result[i].Add(tf * idf[Vocabulary[i]]);
+        }
+        return Matrix_Result;
+
+    }
+    //metodo que calcula el tf del query
+    public static List<double> TF_Query(List<string> query, List<string> vocabulary)
+    {
+        double count = 0;
+        List<double> Query_TF = new List<double>();
+        for (int i = 0; i < vocabulary.Count; i++)
+        {
+            for (int j = 0; j < query.Count; j++)
+            {
+                if (vocabulary[i] == query[j])
+                {
+                    count += 1;
+                }
+                if (j == query.Count - 1)
+                {
+                    Query_TF[i] = count;
+                }
+            }
+        }
+        return Query_TF;
+    }
+    //guardamos en un lista el idf del query
+    public static List<double> IDF_Query(List<string> query, Dictionary<string, double> IDF_Word)
+    {
+        List<double> Query_IDF = new();
+        for (int i = 0; i < query.Count; i++)
+        {
+            Query_IDF.Add(IDF_Word[query[i]]);
+        }
+        return Query_IDF;
+    }
+    //empezamos con el calculo de la similitud de coseno, para eso empezamos calculando la magnitud de los vectores
+    public static double Vector_Magnitude(List<double> Matrix_TF_IDF)
+    {
+        double result = 0;
+        for (int i = 0; i < Matrix_TF_IDF.Count; i++)
+        {
+
+
+            result += Matrix_TF_IDF[i] * Matrix_TF_IDF[i];
+        }
+        return Math.Sqrt(result);
     }
 
-    //calcula el idf de los documento
-    public static /*double[][]*/  void Inverse_Document_Frencuency(string[] documents, List<string> vocabulary)
+
+    //metodo para calcular la similitud de coseno entre un documento y el query
+    public static double Cosine_Similarity(List<double> doc1, List<double> query, double Magnitude_Doc1, double Magnitude_query)
     {
+        double Similitud_Coseno = 0;
+        double valor_vectores = 0;
+        for (int i = 0; i < doc1.Count; i++)
+        {
+            valor_vectores += doc1[i] * query[i];
+        }
+        Similitud_Coseno = valor_vectores / (Magnitude_Doc1 * Magnitude_query);
+        return Similitud_Coseno;
     }
+    //metodo que devuelve la matriz del snippet de un documento donde cada sublista es una oracion de del docuemnto
+    public static List<List<string>> Matrix_Snippet(List<string> Document)
+    {
+        List<List<string>> Snippet = new List<List<string>>();
+        string Actual_document;
+        string[] Document_Split_Into_Sentences;
+        for (int i = 0; i < Document.Count; i++)
+        {
+            for (int j = 0; j < Document.Count; j++)
+            {
+                Actual_document = Document[j];
+                Document_Split_Into_Sentences = Actual_document.Split(".");
+                Snippet[i][j] = Document_Split_Into_Sentences[j];
+            }
+        }
+        return Snippet;
+    }
+    //metodo que devuelve una lista de lista del tf por el idf del snippet
+    public static List<List<double>> Snippet_TF_IDF(List<List<string>> Snippet, List<string> Vocabulary)
+    {
+        List<List<double>> Snippet_TF_IDF = new List<List<double>>();
+        double Tf = 0;
+        var Idf = Convert_To_Diccionary_IDF(Vocabulary, Snippet);
+        for (int i = 0; i < Snippet.Count; i++)
+        {
+            Tf = Term_Frequency(Snippet, Vocabulary[i]);
+            Snippet_TF_IDF[i].Add(Tf * Idf[Vocabulary[i]]);
+        }
+        return Snippet_TF_IDF;
+    }
+    //metodo que devuelve un diccionario de los documentos a devolver con usu titulo
+    public static Dictionary<string, string> Documents_with_Title(List<string> Documents, Dictionary<string, string> Document_Title)
+    {
+        Dictionary<string, string> Documents_with_Title = new Dictionary<string, string>();
+        for (int i = 0; i < Documents.Count; i++)
+        {
+            Documents_with_Title.Add(Documents[i], Document_Title[Documents[i]]);
+        }
+        return Documents_with_Title;
+
+    }
+    //metodo que devuelve un array de searchItem de cada documento a devolver
+    public static SearchItem[] Result(string[] title, double[] score, string snippet, List<string> Return_Documents)
+    {
+        List<SearchItem> result = new List<SearchItem>();
+        SearchItem Preresult;
+        for (int i = 0; i < Return_Documents.Count; i++)
+        {
+            Preresult = new SearchItem(title[i], snippet, score[i]);
+            result.Add(Preresult);
+        }
+        return result.ToArray();
+
+    }
+
+
+
+
+
+
 }
 
 
